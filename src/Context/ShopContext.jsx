ee08b51ce26831db52c5b57099b46e0f53ec415b
faaiz251 <mohdfaaizazeem@gmail.com>
2025-03-14 14:28:18 +0530

    product page done

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,11 +1,20 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import all_product from '../Components/Assests/all_product';
 
 export const ShopContext = createContext(null);
 
+const getDefaultCart = () => {
+    let cart = {};
+    for (let index = 0; index < all_product.length+1; index++) {
+        cart[index] = 0;
+    }
+    return cart;
+}
+
 const ShopContextProvider = (props) => {
     const contextvalue = {all_product};
-
+    const [cartItems,setCartItems] = useState(getDefaultCart());
+  
     return (
         <ShopContext.Provider value ={contextvalue}>
             {props.children}
